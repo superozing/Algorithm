d077251937c6d72089aa62e1a548cc654ece6a10
superozing <127123002+superozing@users.noreply.github.com>
2024-10-25 17:22:12 +0900

    [Silver IV] Title: 동전 0, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,54 @@
+///////////////////////////////////
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+
+class Boj
+{
+private:
+    int N, K; // 동전 종류의 개수, 목표 원
+    vector<int> coins;
+public:
+    void input()
+    {
+        cin >> N >> K;
+        coins.resize(N);
+        for (int i = 0; i < N; ++i)
+            cin >> coins[i];
+    }
+
+    void progress()
+    {
+        int r = N - 1;
+        int answer = 0;
+        while (K != 0)
+            coins[r] <= K ? K -= coins[r], ++answer : --r;
+        cout << answer;
+    }
+
+private:
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Boj boj;
+
+    boj.input();
+    boj.progress();
+
+    return 0;
+}
+
