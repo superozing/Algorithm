446e16971321f3bdb95f63b442d1abbad4ee751d
superozing <127123002+superozing@users.noreply.github.com>
2024-10-30 09:37:38 +0900

    [Gold IV] Title: 사이클 게임, Time: 100 ms, Memory: 9708 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -0,0 +1,91 @@
+
+///////////////////////////////////
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+
+class Boj
+{
+private:
+    int N, M; // 점의 개수, 진행된 차례의 수
+
+    vector<int> vtx; // 인덱스: 점의 고유 번호, value: 점이 연결된 곳
+
+public:
+    void input()
+    {
+        cin >> N >> M;
+        
+        vtx.resize(N);
+
+        for (int i = 0; i < N; ++i)
+            vtx[i] = i;
+    }
+
+    void progress()
+    {
+        int a, b;
+        for (int testcase = 1; testcase <= M; ++testcase)
+        {
+            cin >> a >> b;
+
+            if (Union(a, b))
+            {
+                cout << testcase;
+                return;
+            }
+        }
+
+        cout << 0;
+        return;
+    }
+
+private:
+
+    // 사이클 여부 반환
+    bool Union(int a, int b)
+    {
+        int _a = Find(a);
+        int _b = Find(b);
+
+        if (vtx[_a] == _b || vtx[_b] == _a)
+            return true;
+
+        vtx[_a] = _b;
+
+        return false;
+    }
+
+    int Find(int a)
+    {
+        if (a != vtx[a])
+            return vtx[a] = Find(vtx[a]);
+
+        return a;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    //cout.tie(NULL);
+
+    Boj boj;
+
+    boj.input();
+    boj.progress();
+
+    return 0;
+}
+
