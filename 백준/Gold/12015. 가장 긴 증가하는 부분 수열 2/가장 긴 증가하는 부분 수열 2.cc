0a83cc2e72a9546c607487a4a5ffa6abb6cfe9b0
superozing <127123002+superozing@users.noreply.github.com>
2024-11-08 09:27:20 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 140 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,75 +1,67 @@
+///////////////////////////////////////////
 #include <iostream>
-#include <vector>
-
 #include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
 
 using namespace std;
 
-class BAEKJOON
+class Boj
 {
-	int			N;	 // 수열 A의 크기
-
-	vector<int> arr; // 수열
-	vector<int> res; // 결과 수열이 오름차순으로 담길 벡터 res
-
-public:
-	void init();
-	void progress();
+private:
+    int N;
+    vector<int> nums;
 
 public:
-	BAEKJOON() { init(); }
-	~BAEKJOON() {}
+
+    void input()
+    {
+        cin >> N;
+
+    }
+
+    void progress()
+    {
+        int in;
+        while (N--)
+        {
+            cin >> in;
+
+            auto it = lower_bound(nums.begin(), nums.end(), in);
+
+            if (it == nums.end())
+                nums.push_back(in);
+            else
+                nums[it - nums.begin()] = min(nums[it - nums.begin()], in);
+        }
+
+        cout << nums.size();
+    }
+
+private:
+
 };
-
-
-void BAEKJOON::init()
-{
-	cin >> N;
-
-	arr.resize(N);
-
-	for (int i = 0; i < N; ++i)
-		cin >> arr[i];
-}
-
-void BAEKJOON::progress()
-{
-	int arrCount = 0;
-
-	// 첫 원소를 담아서 for문 안에서 비교해줄 수 있도록 함
-	res.push_back(arr[0]);
-	++arrCount;
-
-	for (int i = 1; i < N; ++i)
-	{
-		if (res.back() < arr[i])
-		{
-			// 만약 arr[i]가 마지막 원소보다 클 경우에는 정상적으로 오름차순으로 정렬되고 있기에 뒤에 넣어줌.
-			res.push_back(arr[i]);
-
-			// 수열의 개수가 1개 증가
-			++arrCount;
-		}
-		else
-		{
-			// 만약 arr[i]가 마지막 원소보다 작을 경우에는, 
-			// arr[i]보다 처음으로 큰 부분의 인덱스를 찾아서 오름차순으로 정렬시켜줌.
-			int idx = lower_bound(res.begin(), res.end(), arr[i]) - res.begin();
-			res[idx] = arr[i];
-		}
-	}
-
-	// res에 들어있는 배열의 원소가 LIS를 구성하는 원소는 아니라는 점을 주의하래요.
-	// 먼저 들어온 원소보다 lower_bound로 찾은 위치가 더 이전일 수 있기 때문이래요.
-
-	cout << arrCount;
-
-}
 
 int main()
 {
-	BAEKJOON b;
-	b.progress();
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	return 0;
+    Boj boj;
+
+    boj.input();
+    boj.progress();
+
+    return 0;
 }
+
