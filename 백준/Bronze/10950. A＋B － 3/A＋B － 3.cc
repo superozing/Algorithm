c8b998d1ce2dac45941c5bc44bcda12337e96818
superozing <127123002+superozing@users.noreply.github.com>
2024-11-14 15:05:17 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,57 @@
+///////////////////////////////////////////
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+
+class Boj
+{
+private:
+    int N;
+
+public:
+
+    void input()
+    {
+        cin >> N;
+    }
+
+    void progress()
+    {
+        int a, b;
+        while (N--)
+        {
+            cin >> a >> b;
+            printf("%d\n", a + b);
+        }
+    }
+
+private:
+
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Boj boj;
+
+    boj.input();
+    boj.progress();
+
+    return 0;
+}
+
