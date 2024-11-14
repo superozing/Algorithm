31b0d3da5224fcfe0de72cfc1ddc2b6ae76c328f
superozing <127123002+superozing@users.noreply.github.com>
2024-11-14 15:45:52 +0900

    [Bronze II] Title: 단어의 개수, Time: 4 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,75 @@
+///////////////////////////////////////////
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+
+class Boj
+{
+private:
+    string s;
+    int ans;
+
+public:
+
+    void input()
+    {
+        getline(cin, s);
+        ans = 0;
+    }
+
+    void progress()
+    {
+        if (s.empty())
+        {
+            printf("0");
+            return;
+        }
+        
+        int i = 0;
+
+        if (s.front() == ' ')
+            i = 1;
+
+
+        if (s.back() != ' ')
+            s += ' ';
+
+        for (; i < s.length(); ++i)
+        {
+            if (s[i] == ' ')
+                ++ans;
+        }
+
+        printf("%d", ans);
+    } 
+
+private:
+
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Boj boj;
+
+    boj.input();
+    boj.progress();
+
+    return 0;
+}
+
