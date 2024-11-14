491c3f146018f6b79c1681c7417d403c520d14f3
superozing <127123002+superozing@users.noreply.github.com>
2024-11-14 14:58:45 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,53 @@
+///////////////////////////////////////////
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+
+class Boj
+{
+private:
+    int n;
+
+public:
+
+    void input()
+    {
+        cin >> n;
+    }
+
+    void progress()
+    {
+        for (int i = 1; i <= n; ++i)
+            printf("%d\n", i);
+    }
+
+private:
+
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Boj boj;
+
+    boj.input();
+    boj.progress();
+
+    return 0;
+}
+
