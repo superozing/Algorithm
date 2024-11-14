12c0163f330c0e55968ad6df50c15ae7758de9e6
superozing <127123002+superozing@users.noreply.github.com>
2024-11-14 15:08:17 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,52 @@
+///////////////////////////////////////////
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+
+class Boj
+{
+private:
+    char c;
+
+public:
+
+    void input()
+    {
+        cin >> c;
+    }
+
+    void progress()
+    {
+        printf("%d", c);
+    }
+
+private:
+
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Boj boj;
+
+    boj.input();
+    boj.progress();
+
+    return 0;
+}
+
