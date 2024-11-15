3b04530666699865ca3ada133f82aeade4056e72
superozing <127123002+superozing@users.noreply.github.com>
2024-11-15 16:14:10 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,68 @@
+///////////////////////////////////////////
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+
+using namespace std;
+
+struct pos
+{
+    int x, y;
+};
+
+class Boj
+{
+private:
+    int N;
+    vector<pos> v;
+
+public:
+
+    bool operator() (pos& a, pos& b)
+    {
+        return a.y == b.y ? a.x < b.x : a.y < b.y;
+    }
+
+    void input()
+    {
+        cin >> N;
+        v.resize(N);
+        for (int i = 0; i < N; ++i)
+            cin >> v[i].x >> v[i].y;
+    }
+
+    void progress()
+    {
+        sort(v.begin(), v.end(), Boj());
+        for (auto& it : v)
+            printf("%d %d\n", it.x, it.y);
+    }
+
+private:
+
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Boj boj;
+
+    boj.input();
+    boj.progress();
+
+    return 0;
+}
+
