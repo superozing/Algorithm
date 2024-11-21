a728f2acbcff2fd168aa8e3384bdd06d1a084477
superozing <127123002+superozing@users.noreply.github.com>
2024-11-21 16:48:03 +0900

    [Silver II] Title: 나무 자르기, Time: 152 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,68 +1,101 @@
-// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <cmath>
 
 using namespace std;
 
+#define endl ("\n")
+
 class Boj
 {
+private:
+    int N; // 나무의 수
+    int M; // 적어도 M미터의 나무
+
+    int maxHeight;
+
+    vector<int> logs;
+
 public:
-    long long n; // 나무의 수
-    long long length; // 필요한 나무의 길이
-    vector<long long> trees;
 
     void input()
     {
-        cin >> n >> length;
-        trees.resize(n);
-        for (long long i = 0; i < n; ++i)
+        cin >> N >> M;
+        
+        logs.resize(N);
+
+        maxHeight = 0;
+        
+        for (int i = 0; i < N; ++i)
         {
-            cin >> trees[i];
+            cin >> logs[i];
+            maxHeight = max(maxHeight, logs[i]);
         }
     }
 
     void progress()
     {
-        long long l = 0;
-        long long r = 2000000000; // 20억
+        int l = 0;
+        int r = maxHeight;
 
         while (l <= r)
         {
-            long long m = (l + r) / 2;
-            long long cutTree = GetCuttingTree(m);
-            if (cutTree < length)
-                r = m - 1;
-            else if (cutTree > length)
-                l = m + 1;
-            else
+            int m = (l + r) / 2;
+
+            long long cut = cutting(m);
+
+            if (M == cut)
             {
                 cout << m;
                 return;
             }
+            else if (M > cut)
+                r = m - 1;
+            else
+                l = m + 1;
         }
-        cout << r;
-    }
 
+        cout << r;
+        return;
+    }
 
 private:
-    long long GetCuttingTree(long long height)
+
+    long long cutting(int m)
     {
-        long long acc = 0;
+        long long treeSize = 0;
 
-        for (long long i = 0; i < n; ++i)
-            acc += (trees[i] - height > 0) ? trees[i] - height : 0;
+        for (int log : logs)
+            treeSize += log - m < 0 ? 0 : log - m;
 
-        return acc;
+        return treeSize;
     }
+
 };
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     Boj boj;
+
     boj.input();
     boj.progress();
 
     return 0;
 }
+
