4846be123551ca595760b2010f01ac4664d35dd4
superozing <127123002+superozing@users.noreply.github.com>
2024-11-22 10:09:28 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 12 ms, Memory: 2828 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -1,88 +1,99 @@
+///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <stack>
 #include <set>
 #include <map>
 #include <string>
 #include <queue>
+#include <deque>
 #include <unordered_map>
 #include <unordered_set>
+#include <bitset>
 #include <cmath>
 
 using namespace std;
 
+#define endl ("\n")
+
+struct pos
+{
+    int y, x;
+};
+
 class Boj
 {
 private:
-    int N, M; // NxM
-    int sy, sx; // 시작 y, 시작 x
-    vector<string> grid;
+    int Y, X;
 
-    int dy[4]{ 0, 1, 0, -1 };
-    int dx[4]{ 1, 0, -1, 0 };
+    vector<string> grid;
+    vector<vector<bool>> visited;
+
+    int iy;
+    int ix;
+
 public:
 
     void input()
     {
-        sy = -1;
+        cin >> Y >> X;
 
-        cin >> N >> M;
-        grid.resize(N);
+        grid.resize(Y, string(X, ' '));
+        visited.resize(Y, vector<bool>(X));
 
-        for (int i = 0; i < N; ++i)
+        for (int i = 0; i < Y; ++i)
+        for (int j = 0; j < X; ++j)
         {
-            cin >> grid[i];
-            
-            // 아직 도연이 위치를 찾지 못한 경우
-            if (sy == -1)
+            cin >> grid[i][j];
+            if (grid[i][j] == 'I')
             {
-                for (int j = 0; j < M; ++j)
+                iy = i;
+                ix = j;
+            }
+        }
+    }
+
+    int dy[4]{ 0, -1, 0, 1 };
+    int dx[4]{ -1, 0, 1, 0 };
+
+    void progress()
+    {
+        int answer = 0;
+        queue<pos> q;
+
+        q.push({ iy, ix});
+
+        while (!q.empty())
+        {
+            pos cur = q.front();
+            q.pop();
+            
+            for (int i = 0; i < 4; ++i)
+            {
+                pos np = cur;
+                np.y += dy[i];
+                np.x += dx[i];
+
+                if (np.x >= 0 && np.x < X &&
+                    np.y >= 0 && np.y < Y &&
+                    visited[np.y][np.x] == false && grid[np.y][np.x] != 'X')
                 {
-                    if ('I' == grid[i][j])
-                    {
-                        sy = i;
-                        sx = j;
-                        break;
-                    }
+                    visited[np.y][np.x] = true;
+                    
+                    if (grid[np.y][np.x] == 'P')
+                        ++answer;
+
+                    q.push(np);
                 }
             }
         }
 
+        answer == 0 ? cout << "TT" : cout << answer;
     }
 
-    void progress()
-    {
-        int ans = dfs(sy, sx);
-
-        if (ans == 0)
-            cout << "TT";
-        else
-            cout << ans;
-    }
-
-    int dfs(int y, int x)
-    {
-        int ret = 0;
-
-        // 도연이가 사람을 만난 경우
-        if (grid[y][x] == 'P')
-            ++ret;
-
-        // visited 처리
-        grid[y][x] = 'X';
-
-        for (int i = 0; i < 4; ++i)
-        {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if (ny >= 0 && ny < N && nx >= 0 && nx < M && grid[ny][nx] != 'X')
-                ret += dfs(ny, nx);
-        }
-
-        return ret;
-    }
+private:
 
 };
 
@@ -90,8 +101,10 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
     Boj boj;
+
     boj.input();
     boj.progress();
 
