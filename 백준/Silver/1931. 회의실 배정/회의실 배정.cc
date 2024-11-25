4d9a9f0c767fa64739c89ef0e1418a522c18c899
superozing <127123002+superozing@users.noreply.github.com>
2024-11-25 09:28:24 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,79 +1,83 @@
-//////////////////////////////////////////
-
+///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <stack>
 #include <set>
 #include <map>
 #include <string>
 #include <queue>
+#include <deque>
 #include <unordered_map>
 #include <unordered_set>
+#include <bitset>
 #include <cmath>
 
 using namespace std;
 
-struct meet
+#define endl ("\n")
+
+struct Meeting
 {
-    int start;
-    int end;
+    int s, e;
 };
 
 struct cmp
 {
-    bool operator ()(meet& a, meet& b)
+    bool operator() (Meeting& a, Meeting& b)
     {
-        return a.end != b.end ? a.end > b.end : a.start > b.start;
+        // 끝나는 시간을 기준으로 정렬
+        return a.e == b.e ? a.s < b.s : a.e < b.e;
     }
 };
 
 class Boj
 {
 private:
-    int N;
-    priority_queue<meet, vector<meet>, cmp> meetings;
+    int N; // 회의의 수
+
+    vector<Meeting> meetings;
 
 public:
+
     void input()
     {
         cin >> N;
-        meet newMeeting;
 
-        for (int i = 0; i < N; ++i)
-        {
-            cin >> newMeeting.start >> newMeeting.end;
-            meetings.push(newMeeting);
-        }
+        meetings.resize(N);
+        for (int i = 0; i < meetings.size(); ++i)
+            cin >> meetings[i].s >> meetings[i].e;
+
+        sort(meetings.begin(), meetings.end(), cmp());
     }
 
     void progress()
     {
-        int curTime = 0;
+        int endTime = 0;
         int answer = 0;
-        while (!meetings.empty())
-        {
-            auto cur = meetings.top();
-            meetings.pop();
 
-            // 이미 시작시간이 지난 회의
-            if (curTime > cur.start)
-                continue;
-            //cout << cur.start << ", end:" << cur.end << endl;
-            ++answer;
-            curTime = cur.end;
+        for (Meeting& meet : meetings)
+        {
+            if (meet.s >= endTime)
+            {
+                ++answer;
+                endTime = meet.e;
+            }
         }
 
         cout << answer;
     }
 
 private:
+
 };
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
     Boj boj;
 
