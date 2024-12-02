f9062f686796a04317c52577470d3a8670c989d1
superozing <127123002+superozing@users.noreply.github.com>
2024-12-02 09:42:24 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2928 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,79 +1,73 @@
-//////////////////////////////////////////
-// 평범한 배낭
+///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <stack>
 #include <set>
 #include <map>
 #include <string>
 #include <queue>
+#include <deque>
 #include <unordered_map>
 #include <unordered_set>
+#include <bitset>
 #include <cmath>
 
 using namespace std;
 
-constexpr auto INF = 999999999;
+#define endl ("\n")
+
+struct item
+{
+    int w, p;
+};
 
 class Boj
 {
 private:
-    int N; // 물건의 개수
-    int K; // 최대 무게
+    int N, K; // 물품의 수, 준서가 버틸 수 있는 무게
 
-    vector<int> weight; // 물건의 무게
-    vector<int> value; // 물건의 가치
+    vector<item> items;
+    vector<int> dp;
 
 public:
+
     void input()
     {
         cin >> N >> K;
+        items.resize(N);
+        dp.resize(K + 1);
 
-        weight.resize(N + 1);
-        value.resize(N + 1);
-
-        for (int i = 1; i <= N; ++i)
-        {
-            cin >> weight[i];
-            cin >> value[i];
-        }
+        for (int i = 0; i < items.size(); ++i)
+            cin >> items[i].w >> items[i].p;
     }
 
     void progress()
     {
-        // 해당 물건을 배낭에 넣는가?
-
-        // 가방의 무게가 현재 내가 버틸 수 있는 무게보다 무거우면 배낭에 넣을 수 없다.
-        // -> 현재의 K보다 물건의 무게 W가 작으면 배낭에 넣을 수 없다.
-        // 물건을 넣었을 때의 가치 합이 이전 최대값보다 적다면 해당 물건을 넣을 필요가 없다.
-
-        // 무게를 인덱스 기준으로 잡음
-        vector<vector<int>> dp(N + 1, vector<int>(K + 1));
-
-        // 무게dp는 순차적으로 물건 A까지 돌렸을 때 최대 가치
-        // i: 물건
-        // j: 현재 들고 있는 무게
-        for (int i = 1; i <= N; ++i)
-        for (int j = 1; j <= K; ++j)
+        for (item& it : items)
         {
-            // 현재 무게가 물건의 무게보다는 커야 한다
-            if (j >= weight[i])
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
-            // dp[i - 1][j] 이전까지의 최대 가치
-            // dp[i - 1][j - weight[i]] + value[i] 무게를 넣은 가치
-            else // 물건을 더 넣을 수 없다.
-                dp[i][j] = dp[i - 1][j];
+            auto prevdp = dp;
+
+            for (int i = it.w; i <= K; ++i)
+                dp[i] = max(prevdp[i], prevdp[i - it.w] + it.p);
         }
 
-        cout << dp[N][K];
-
+        cout << dp.back();
     }
+
+private:
+
 };
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     Boj boj;
+
     boj.input();
     boj.progress();
 
