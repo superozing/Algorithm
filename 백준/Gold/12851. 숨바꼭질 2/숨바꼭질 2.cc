0456a535c347c52700a666b0dde4c67bf2a89e74
superozing <127123002+superozing@users.noreply.github.com>
2024-12-05 08:59:14 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 8 ms, Memory: 3736 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -28,7 +28,7 @@ class Boj
 private:
     int N, K; // 수빈이의 위치, 동생의 위치
 
-    vector<int> visited;
+    vector<int> minTime;
 
 public:
 
@@ -36,19 +36,18 @@ public:
     {
         cin >> N >> K;
 
-        visited.resize(100001, 1e9);
+        minTime.resize(100001, 1e9);
     }
 
     void progress()
     {
         int answer = 0; // 개수
-        int answerWeight = 0; // 가중치
         
         queue<pos> q;
 
         q.push({ N, 0 });
 
-        visited[N] = 0;
+        minTime[N] = 0;
 
         while (!q.empty())
         {
@@ -56,55 +55,36 @@ public:
             q.pop();
 
             if (cur.x == K)
-            {
-                if (answer == 0)
-                {
-                    answerWeight = cur.c;
-                    ++answer;
-                }
-                else if (answerWeight == cur.c)
-                {
-                    ++answer;
-                }
-                else if (answerWeight > cur.c)
-                {
-                    answerWeight = cur.c;
-                    answer = 1;
-                }
-                else
-                {
-                    break;
-                }
-            }
+                ++answer;
 
             cur.c++;
 
             pos next = cur;
             next.x += 1;
-            if (next.x <= 100000 && visited[next.x] >= next.c)
+            if (next.x <= 100000 && minTime[next.x] >= next.c)
             {
-                visited[next.x] = next.c;
+                minTime[next.x] = next.c;
                 q.push(next);
             }
             
             next = cur;
             next.x -= 1;
-            if (next.x >= 0 && visited[next.x] >= next.c)
+            if (next.x >= 0 && minTime[next.x] >= next.c)
             {
-                visited[next.x] = next.c;
+                minTime[next.x] = next.c;
                 q.push(next);
             }
 
             next = cur;
             next.x *= 2;
-            if (next.x <= 100000 && visited[next.x] >= next.c)
+            if (next.x <= 100000 && minTime[next.x] >= next.c)
             {
-                visited[next.x] = next.c;
+                minTime[next.x] = next.c;
                 q.push(next);
             }
         }
 
-        cout << answerWeight << endl << answer;
+        cout << minTime[K] << endl << answer;
         return;
     }
 
