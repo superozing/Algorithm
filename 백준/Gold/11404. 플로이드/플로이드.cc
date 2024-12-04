31c03e90c39feb606445af359390a8e6c4e92954
superozing <127123002+superozing@users.noreply.github.com>
2024-12-05 08:16:51 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,76 +1,75 @@
-
 ///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <stack>
 #include <set>
 #include <map>
 #include <string>
 #include <queue>
+#include <deque>
 #include <unordered_map>
 #include <unordered_set>
+#include <bitset>
 #include <cmath>
 
 using namespace std;
 
-constexpr int INF = 1e9;
+#define endl ("\n")
 
 class Boj
 {
 private:
-    int N; // 도시 개수
-    int M; // 버스 개수
+    int N, M; // 도시의 개수, 버스의 개수
 
-    vector<vector<int>> graph; // inf로 초기화 하고 가중치를 담으면 되겠네요.
-
+    vector<vector<int>> dp;
 
 public:
+
     void input()
     {
         cin >> N >> M;
-        graph.resize(N + 1, vector<int>(N + 1, INF));
+
+        dp.resize(N + 1, vector<int>(N + 1, 1e9));
 
         int s, e, w;
-        for (int i = 0; i < M; ++i)
+        while (M--)
         {
             cin >> s >> e >> w;
-            graph[s][e] = min(w, graph[s][e]);
+            dp[s][e] = min(dp[s][e], w);
         }
 
         for (int i = 1; i <= N; ++i)
-            graph[i][i] = 0;
+            dp[i][i] = 0;
     }
 
     void progress()
     {
-        // n: 거쳐갈 노드
-        // i: 시작 노드
-        // j: 도착 노드
-        for (int n = 1; n <= N; ++n)
-            for (int i = 1; i <= N; ++i)
-                for (int j = 1; j <= N; ++j)
-                    graph[i][j] = min(graph[i][j], graph[i][n] + graph[n][j]);
+        for (int n = 1; n <= N; ++n) // 중간에 거쳐갈 노드
+        for (int i = 1; i <= N; ++i) // 시작 노드
+        for (int j = 1; j <= N; ++j) // 도착 노드
+            dp[i][j] = min(dp[i][j], dp[i][n] + dp[n][j]);
 
-        // 출력
         for (int i = 1; i <= N; ++i)
         {
             for (int j = 1; j <= N; ++j)
-                printf("%d ", graph[i][j] >= INF ? 0 : graph[i][j]);
+                cout << (dp[i][j] == 1e9 ? 0 : dp[i][j]) << " ";
 
-            printf("\n");
+            cout << endl;
         }
     }
 
 private:
-    
+
+
 };
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    //cout.tie(NULL);
+    cout.tie(NULL);
 
     Boj boj;
 
@@ -79,5 +78,4 @@ int main()
 
     return 0;
 }
-
 
