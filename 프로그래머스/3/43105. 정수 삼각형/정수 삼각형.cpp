f855e815c6abf6454e7d777e6233c68dda176686
superozing <127123002+superozing@users.noreply.github.com>
2025-01-02 10:05:57 +0900

    [level 3] Title: 정수 삼각형, Time: 0.41 ms, Memory: 7.73 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -1,40 +1,23 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int solution(vector<vector<int>> tri) 
+int solution(vector<vector<int>> triangle) 
 {
-    int ans = 0;
+    int answer = 0;
     
-    vector<vector<int>> dp = tri;
-    
-    // 삼각형 깊이가 1, 2인 경우의 예외처리가 필요해요.
-    dp[1][0] += dp[0][0];
-    dp[1][1] += dp[0][0];
-    
-    for (int i = 2; i < dp.size(); ++i) // 깊이
+    for (int i = 1; i < triangle.size(); ++i)
+    for (int j = 0; j < triangle[i].size(); ++j)
     {
-        //cout << i << "층: ";
-        for (int j = 0; j < dp[i].size(); ++j) // 넓이
-        {
-            // 무조건 아래로 내려갈 수록 커진다
-            // 아 근데 비교를 해야 하긴 하네요.
-            // 1. j == 0
-            if (j == 0)
-                dp[i][0] += dp[i - 1][0];
-            // 2. j == dp[i].size() - 1
-            else if (j == dp[i].size() - 1)
-                dp[i][j] += dp[i - 1][j - 1];
-            // 3. else
-            else
-                dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j]);
-            
-            ans = max(dp[i][j], ans);
-        }
-        //cout << endl;
+        if (j == 0)
+            triangle[i][j] += triangle[i - 1][0];
+        else if (j == triangle[i].size() - 1)
+            triangle[i][j] += triangle[i - 1][j - 1];
+        else
+            triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j]);
     }
-
-    return ans;
+    
+    return *max_element(begin(triangle.back()), end(triangle.back()));
 }
