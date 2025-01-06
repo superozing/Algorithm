1039b954c99b40e6e8bd63bba69cda2f36051079
superozing <127123002+superozing@users.noreply.github.com>
2025-01-06 09:36:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.23 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,62 +1,47 @@
 #include <vector>
-#include <list>
+#include <queue>
 using namespace std;
 
 struct pos
 {
-    int y;
-    int x;
-    int cnt;
-    pos() {}
-    pos(int _y, int _x, int _c) : y(_y), x(_x), cnt(_c) {}
+    int y, x, c;
 };
 
 int solution(vector<vector<int>> maps)
 {
-    int answer = 0;
-    // 0은 벽, 1은 지나갈 수 있는 공간.
-    
-    list<pos> q;
-    int pX[4] {-1, 0, 1, 0};
-    int pY[4] { 0, -1, 0, 1};
-    
-    q.push_back({0, 0, 1});
-    maps[0][0] = false;
-    // 원본 수정해도 될 것 같아요.
+    int dy[4] {1, 0, -1, 0};
+    int dx[4] {0, 1, 0, -1};
+    queue<pos> q;
+        
+    q.push({0, 0, 1});
+    maps[0][0] = 0;
     
     while (!q.empty())
     {
         pos cur = q.front();
-        q.pop_front();
+        q.pop();
         
-        cur.cnt += 1;
+        ++cur.c;
         
-        for (int i  =0; i < 4; ++i)
+        for (int i = 0; i < 4; ++i)
         {
-            pos pnew = cur;
-            pnew.y += pY[i];
-            pnew.x += pX[i];
+            pos next = cur;
+            next.y += dy[i];
+            next.x += dx[i];
             
-            if (pnew.y < maps.size() && pnew.x < maps[0].size() &&
-                pnew.y >= 0 && pnew.x >= 0 &&
-                maps[pnew.y][pnew.x])
+            if (next.x >= 0 && next.x < maps[0].size() && 
+               next.y >= 0 && next.y < maps.size() &&
+               maps[next.y][next.x])
             {
-                if (maps.size() - 1 == pnew.y && maps[0].size() - 1 == pnew.x)
-                    return pnew.cnt;
+                if (next.y == maps.size() - 1 &&
+                    next.x == maps[0].size() - 1)
+                    return next.c;
                 
-                q.push_back(pnew);
-                maps[pnew.y][pnew.x] = false;
+                maps[next.y][next.x] = 0;
+                q.push(next);
             }
         }
-        
     }
-    
-        
-    
-    
-    
-    
-    
     
     return -1;
 }
