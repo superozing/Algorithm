9fda1435c568cc4bc5fc93b8ac5ff5f59458df88
superozing <127123002+superozing@users.noreply.github.com>
2025-01-14 08:16:36 +0900

    [Silver V] Title: 영화감독 숌, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,87 +1,45 @@
-///////////////////////////////////////////
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <list>
-#include <stack>
-#include <set>
-#include <map>
-#include <string>
-#include <queue>
-#include <deque>
-#include <unordered_map>
-#include <unordered_set>
-#include <cmath>
 
 using namespace std;
 
-class Boj
-{
-private:
-    int N;
-
-public:
-
-    void input()
-    {
-        cin >> N;
-    }
-
-    void progress()
-    {
-        // 종말의 수: 어떤 수에 적어도 3개 이상 6이 연속으로 들어간 수
-        // ex) 1666, 2666, 3666 등
-        // N 번째 영화의 제목에 들어간 수.
-
-
-        int answer = 0;
-
-        for (int i = 0; i < N;)
-        {
-            ++answer;
-            string s = to_string(answer);
-            int sixCnt = 0;
-
-            // 문자열을 돌면서 6이 3개인지 검사
-            for (char c : s)
-            {
-                if (c == '6')
-                {
-                    ++sixCnt;
-
-                    if (sixCnt == 3)
-                    {
-                        ++i;
-                        break;
-                    }
-                }
-                else
-                    sixCnt = 0;
-
-
-            }
-
-        }
-
-        cout << answer;
-
-
-    }
-
-private:
-
-};
-
 int main()
 {
+    cin.tie(0);
+    cout.tie(0);
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    Boj boj;
+    int N;
+    cin >> N;
 
-    boj.input();
-    boj.progress();
+    int answer = 0;
+    
+    for (int i = 666; i <= 1e9; ++i)
+    {
+        int sixCnt = 0;
+        int cur = i;
+
+        while (cur != 0)
+        {
+            if (6 == cur % 10)
+                ++sixCnt;
+            else
+                sixCnt = 0;
+
+            cur /= 10;
+
+            if (sixCnt == 3)
+            {
+                ++answer;
+                break;
+            }
+        }
+
+        if (answer == N)
+        {
+            cout << i;
+            break;
+        }
+    }
 
     return 0;
 }
