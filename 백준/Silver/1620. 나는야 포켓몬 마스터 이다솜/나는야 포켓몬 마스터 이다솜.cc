6914a6808dc933178455e6ff369ee1797c416dc4
superozing <127123002+superozing@users.noreply.github.com>
2025-07-11 21:14:55 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 88 ms, Memory: 20668 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,82 +1,42 @@
-///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <stack>
-#include <set>
-#include <map>
-#include <string>
-#include <queue>
-#include <deque>
-#include <unordered_map>
-#include <unordered_set>
-#include <bitset>
 #include <cmath>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#define endl '\n'
 
 using namespace std;
 
-#define endl "\n"
-
-class Boj
-{
-private:
-    int N; // 도감 정보 개수
-    int M; // 문제 개수
-
-    unordered_map<string, int> nameum;
-    unordered_map<int, string> keyum;
-
-public:
-
-    void input()
-    {
-        cin >> N;
-        cin >> M;
-
-        string s;
-        for (int i = 1; i <= N; ++i)
-        {
-            cin >> s;
-            nameum[s] = i;
-            keyum[i] = s;
-        }
-    }
-
-    void progress()
-    {
-        string in;
-        while (M--)
-        {
-            cin >> in;
-
-            if (in[0] <= '9')
-            {
-                int key = stoi(in);
-                cout << keyum[key] << endl;
-            }
-            else
-            {
-                cout << nameum[in] << endl;
-            }
-        }
-    }
-
-private:
-
-};
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    Boj boj;
+	int N, M;
+	cin >> N >> M;
 
-    boj.input();
-    boj.progress();
+	vector<string> v(N + 1);
+	unordered_map<string, int> um;
 
-    return 0;
+	for (int i = 1; i < N + 1; ++i)
+	{
+		cin >> v[i];
+		um.insert({ v[i] , i });
+	}
+
+	while (M--)
+	{
+		string in;
+		cin >> in;
+		if ('0' <= in[0] && in[0] <= '9')
+			cout << v[stoi(in)] << endl;
+		else
+			cout << um[in] << endl;
+	}
+	
+
+	return 0;
 }
-
