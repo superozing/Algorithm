0f652116b784c862fb58d21ac50f3747ca7e7fa1
superozing <127123002+superozing@users.noreply.github.com>
2025-07-12 05:01:20 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,71 +1,38 @@
-///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <stack>
-#include <set>
-#include <map>
-#include <string>
-#include <queue>
-#include <deque>
-#include <unordered_map>
-#include <unordered_set>
-#include <bitset>
 #include <cmath>
+
+#include <vector>
+
+#define endl '\n'
 
 using namespace std;
 
-#define endl ("\n")
-
-class Boj
-{
-private:
-    int N; // 수의 개수
-    int M; // 합을 구해야 하는 구간의 개수
-
-    vector<int> nums;
-    
-public:
-
-    void input()
-    {
-        cin >> N >> M;
-        nums.resize(N + 1);
-
-        for (int i = 1; i < nums.size(); ++i)
-        {
-            cin >> nums[i];
-            nums[i] += nums[i - 1];
-        }
-
-    }
-
-    void progress()
-    {
-        int a, b;
-        while (M--)
-        {
-            cin >> a >> b;
-            cout << nums[b] - nums[a - 1] << endl;
-        }
-    }
-
-private:
-
-};
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int N, M;
+	cin >> N >> M;
 
-    Boj boj;
+	vector<int> dp(N);
 
-    boj.input();
-    boj.progress();
+	cin >> dp[0];
 
-    return 0;
+	for (int i = 1; i < N; ++i)
+	{
+		cin >> dp[i];
+		dp[i] += dp[i - 1];
+	}
+
+	int s, e;
+	while (M--)
+	{
+		cin >> s >> e;
+		cout << dp[e - 1] - (s == 1 ? 0 : dp[s - 2]) << endl;
+	}
+
+	return 0;
 }
-
+ 
