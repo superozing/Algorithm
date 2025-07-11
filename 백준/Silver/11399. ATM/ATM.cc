53f2b53326e71c2b4d5e608a07a63603adfba413
superozing <127123002+superozing@users.noreply.github.com>
2025-07-11 21:54:44 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,59 +1,36 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
+
 #include <vector>
 
-#include <algorithm>
+#define endl '\n'
 
 using namespace std;
 
-class BAEKJOON // ATM
-{
-public:
-    int N; // 사람
-    vector<int> line; // 줄
-
-public:
-    BAEKJOON() { init(); }
-    void init();
-    void progress();
-
-private:
-};
-
-void BAEKJOON::init()
-{
-    cin >> N;
-
-    int buf = 0;
-
-    for (int i = 0; i < N; ++i)
-    {
-        cin >> buf;
-        line.push_back(buf);
-    }
-
-    sort(line.begin(), line.end());
-}
-
-void BAEKJOON::progress()
-{
-    // 정렬된 순서로 걸리는 시간 구하기
-    for (int i = 1; i < line.size(); ++i)
-        line[i] += line[i - 1];
-
-
-    int buf = 0;
-
-    // 구한 값 더하기
-    for (auto& it : line)
-        buf += it;
-
-    cout << buf;
-}
-
 int main()
 {
-    BAEKJOON b;
-    b.progress();
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    return 0;
+	int N;
+	cin >> N;
+
+	vector<int> v(N);
+	for (int& i : v)
+		cin >> i;
+
+	sort(begin(v), end(v));
+
+	int answer = v.front();
+
+	for (int i = 1; i < v.size(); ++i)
+	{
+		v[i] += v[i - 1];
+		answer += v[i];
+	}
+
+	cout << answer;
+
+	return 0;
 }
