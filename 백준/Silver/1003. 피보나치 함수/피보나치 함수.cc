72cda318db277167a19d95051cb11e11a9081b31
superozing <127123002+superozing@users.noreply.github.com>
2025-07-11 22:25:31 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,78 +1,34 @@
-///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <stack>
-#include <set>
-#include <map>
-#include <string>
-#include <queue>
-#include <deque>
-#include <unordered_map>
-#include <unordered_set>
-#include <bitset>
 #include <cmath>
+
+#include <vector>
+
+#define endl '\n'
 
 using namespace std;
 
-#define endl "\n"
-
-struct fib
-{
-    int zero, one;
-
-    fib operator + (fib& other)
-    {
-        return { zero + other.zero, one + other.one };
-    }
-};
-
-class Boj
-{
-private:
-    int T; // 테케 개수
-    vector<fib> dp;
-
-public:
-
-    void input()
-    {
-        cin >> T;
-        dp.resize(41);
-        dp[0] = { 1, 0 };
-        dp[1] = { 0, 1 };
-        dp[2] = { 1, 1 };
-    }
-
-    void progress()
-    {
-        for (int i = 3; i < 41; ++i)
-            dp[i] = dp[i - 1] + dp[i - 2];
-
-        int in;
-        while (T--)
-        {
-            cin >> in;
-            cout << dp[in].zero << " " << dp[in].one << endl;
-        }
-    }
-
-private:
-
-};
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    Boj boj;
+	vector<pair<int, int>> dp(41);
+	dp[0] = {1, 0};
+	dp[1] = {0, 1};
 
-    boj.input();
-    boj.progress();
+	for (int i = 2; i < dp.size(); ++i)
+		dp[i] = { dp[i - 2].first + dp[i - 1].first, dp[i - 2].second + dp[i - 1].second };
 
-    return 0;
+	int T;
+	cin >> T;
+
+	int N;
+	while (T--)
+	{
+		cin >> N;
+		cout << dp[N].first << ' ' << dp[N].second << endl;
+	}
+
+	return 0;
 }
-
