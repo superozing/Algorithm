a594f3ea32d499da7c6e5522bc1160869f715924
superozing <127123002+superozing@users.noreply.github.com>
2025-07-12 01:58:06 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,96 +1,36 @@
-///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <stack>
-#include <set>
-#include <map>
-#include <string>
-#include <queue>
-#include <deque>
-#include <unordered_map>
-#include <unordered_set>
-#include <bitset>
 #include <cmath>
+
+#include <vector>
+
+#define endl '\n'
 
 using namespace std;
 
-struct node
-{
-    int n, c;
-};
-
-class Boj
-{
-private:
-    int N; // 계단 수
-
-    vector<int> stairs;
-    vector<int> dp;
-
-public:
-
-    void input()
-    {
-        cin >> N;
-        stairs.resize(N);
-        dp.resize(N);
-
-        for (int i = 0; i < N; ++i)
-            cin >> stairs[i];
-    }
-
-    void progress()
-    {
-        dp[0] = stairs[0];
-
-        if (N == 1)
-        {
-            cout << dp[0];
-            return;
-        }
-
-        dp[1] = stairs[0] + stairs[1];
-        if (N == 2)
-        {
-            cout << dp[1];
-            return;
-        }
-        dp[2] = max(stairs[1] + stairs[2], stairs[0] + stairs[2]);
-        if (N == 3)
-        {
-            cout << dp[2];
-            return;
-        }
-
-
-        //dp[3] = 01 3, 0 23
-        //dp[3] = max(dp[2] + stairs[3], stairs[1] + stairs[2]);
-
-        for (int i = 3; i < N; ++i)
-            dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
-
-
-        cout << dp.back();
-
-    }
-
-private:
-
-};
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    Boj boj;
+	int N;
+	cin >> N;
 
-    boj.input();
-    boj.progress();
+	vector<int> stair(N);
+	vector<int> dp(N);
+	for (int& i : stair)
+		cin >> i;
 
-    return 0;
+	dp[0] = stair[0];
+	dp[1] = stair[0] + stair[1];
+	dp[2] = max(stair[0] + stair[2], stair[1] + stair[2]);
+
+	for (int i = 3; i < dp.size(); ++i)
+		dp[i] = max(dp[i - 3] + stair[i - 1] + stair[i], 
+					dp[i - 2] + stair[i]);
+
+	cout << dp.back();
+
+	return 0;
 }
-
+ 
