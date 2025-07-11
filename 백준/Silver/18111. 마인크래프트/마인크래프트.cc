f640e5dee46d3862ebce0b1c21a5dc2616de3b65
superozing <127123002+superozing@users.noreply.github.com>
2025-07-11 18:57:10 +0900

    [Silver II] Title: 마인크래프트, Time: 100 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.cc
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -8,51 +8,32 @@
 
 using namespace std;
 
-int GetTime(vector<vector<int>>& grid, int N, int M, int B, int height)
+int GetTime(vector<int>& grid, int B, int height)
 {
-	//2초) 좌표 (i, j)의 가장 위에 있는 블록을 제거하여 인벤토리에 넣는다.
-	//1초) 인벤토리에서 블록 하나를 꺼내어 좌표(i, j)의 가장 위에 있는 블록 위에 놓는다.
-
-	// 어떻게 구할 것인가?
-	// 일단 해당 높이로 "무조건" 맞추어야 하기 떄문에,
-	// 해당 높이보다 높은 블럭은 무조건 깎고,
-	// 낮은 블럭은 무조건 채워야 한다.
-
 	int time = 0;
 
-	// 블록 깎아내기
-	for (int y = 0; y < N; ++y)
-	for (int x = 0; x < M; ++x)
+	for (int cur : grid)
 	{
-		if (grid[y][x] <= height)
-			continue;
+		//2초) 좌표 (i, j)의 가장 위에 있는 블록을 제거하여 인벤토리에 넣는다.
+		if (cur > height)
+		{
+			time += (cur - height) * 2;
 
-		// 시간 증가
-		time += (grid[y][x] - height) * 2;
+			// 블록을 인벤토리에 넣기
+			B += cur - height;
+		}
+		//1초) 인벤토리에서 블록 하나를 꺼내어 좌표(i, j)의 가장 위에 있는 블록 위에 놓는다.
+		else if (cur < height)
+		{
+			time += height - cur;
 
-		// 블록을 인벤토리에 넣기
-		B += grid[y][x] - height;
+			// 블록을 놓기
+			B -= height - cur;
+		}
 	}
 
-	// 블록 추가하기
-	for (int y = 0; y < N; ++y)
-	for (int x = 0; x < M; ++x)
-	{
-		if (grid[y][x] >= height)
-			continue;
-
-		// 시간 증가
-		time += height - grid[y][x];
-
-		// 블록을 놓기
-		B -= height - grid[y][x];
-
-		// 블록이 모자란가요?
-		if (B < 0)
-			return -1;
-	}
-
-	return time;
+	// 실패 시 -1
+	return B < 0 ? -1 : time;
 }
 
 int main()
@@ -63,11 +44,10 @@ int main()
 	int N, M, B; // 세로, 가로, 블록의 수
 	cin >> N >> M >> B;
 
-	vector<vector<int>> grid(N, vector<int>(M));
+	vector<int> grid(N * M);
 
 	int l = 256, r = 0;
-	for (auto& vec : grid)
-	for (int& i : vec)
+	for (int& i : grid)
 	{
 		cin >> i;
 		l = min(l, i);
@@ -79,7 +59,7 @@ int main()
 	for (; l <= r; ++l)
 	{
 		int height = l;
-		int curTime = GetTime(grid, N, M, B, height);
+		int curTime = GetTime(grid, B, height);
 
 		if (curTime == -1)
 			break;
