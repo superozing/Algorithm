96ee56fc19fe88dd414f0bf42ad78e94eceafba5
superozing <127123002+superozing@users.noreply.github.com>
2025-07-19 00:32:13 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -17,36 +17,31 @@ int main()
 	cin >> N;
 
 	vector<int> in(N);
-	vector<int> asc(N);
-	vector<int> des(N);
-
-	vector<int> ascStack;
-	vector<int> desStack;
+	vector<int> dp(N);
 
 	for (int& i : in)
 		cin >> i;
+
+	vector<int> ascStack;
+	vector<int> desStack;
 
 	for (int i = 0; i < N; ++i)
 	{
 		if (ascStack.empty() || ascStack.back() < in[i])
 			ascStack.push_back(in[i]);
-		else if (ascStack.back() > in[i])
+		else
 			*lower_bound(begin(ascStack), end(ascStack), in[i]) = in[i];
 		
 		if (desStack.empty() || desStack.back() < in[N - i - 1])
 			desStack.push_back(in[N - i - 1]);
-		else if (desStack.back() > in[N - i - 1])
+		else
 			*lower_bound(begin(desStack), end(desStack), in[N - i - 1]) = in[N - i - 1];
 
-		asc[i] = ascStack.size();
-		des[N - i - 1] = desStack.size();
+		dp[i] += ascStack.size();
+		dp[N - i - 1] += desStack.size();
 	}
 
-	int answer = 0;
-	for (int i = 0; i < N; ++i)
-		answer = max(asc[i] + des[i] - 1, answer);
-
-	cout << answer;
+	cout << *max_element(begin(dp), end(dp)) - 1;
 
 	return 0;
 }
