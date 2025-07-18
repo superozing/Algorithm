8227b21d0bc9536ff05142d7abe6c49b2548b5f0
superozing <127123002+superozing@users.noreply.github.com>
2025-07-19 00:18:08 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -28,71 +28,19 @@ int main()
 
 	for (int i = 0; i < N; ++i)
 	{
-		// 0. 비어있을 경우
-		// 1. (ascending 기준) back()보다 클 경우
-		// push_back()
 		if (ascStack.empty() || ascStack.back() < in[i])
 			ascStack.push_back(in[i]);
-		
-		// 2. back()보다 작을 경우
 		else if (ascStack.back() > in[i])
-		{
-			// - 같은 값이 있다면 continue 해요.
-			// - in[i]보다 큰 값이 나온다면 그 곳에 insert 해요.
-			for (int j = 0; j < ascStack.size(); ++j)
-			{
-				if (ascStack[j] >= in[i])
-				{
-					ascStack[j] = in[i];
-					break;
-				}
-			}
-		}
-
-		// 마지막에 ascStack의 길이만큼 인덱스에 세팅
-		asc[i] = ascStack.size();
-
-
+			*lower_bound(begin(ascStack), end(ascStack), in[i]) = in[i];
+		
 		if (desStack.empty() || desStack.back() < in[N - i - 1])
 			desStack.push_back(in[N - i - 1]);
-
 		else if (desStack.back() > in[N - i - 1])
-		{
-			for (int j = 0; j < desStack.size(); ++j)
-			{
-				if (desStack[j] >= in[N - i - 1])
-				{
-					desStack[j] = in[N - i - 1];
-					break;
-				}
-			}
-		}
+			*lower_bound(begin(desStack), end(desStack), in[N - i - 1]) = in[N - i - 1];
 
-		// 마지막에 ascStack의 길이만큼 인덱스에 세팅
 		asc[i] = ascStack.size();
 		des[N - i - 1] = desStack.size();
 	}
-
-
-	//cout << "ascStack.size(): " << ascStack.size() << endl;
-	//for (int i = 0; i < ascStack.size(); ++i)
-	//	cout << ascStack[i] << ' ';
-	//
-	//cout << endl;
-	//cout << "desStack.size(): " << desStack.size() << endl;
-	//for (int i = 0; i < desStack.size(); ++i)
-	//	cout << desStack[i] << ' ';
-	//
-	//cout << endl;
-	//cout << "asc.size(): " << asc.size() << endl;
-	//for (int i = 0; i < asc.size(); ++i)
-	//	cout << asc[i] << ' ';	
-	//
-	//cout << endl;
-	//cout << "des.size(): " << des.size() << endl;
-	//for (int i = 0; i < des.size(); ++i)
-	//	cout << des[i] << ' ';
-	//cout << endl;
 
 	int answer = 0;
 	for (int i = 0; i < N; ++i)
