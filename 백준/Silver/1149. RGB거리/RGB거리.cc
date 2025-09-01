2227bfa9eebc3e98a583124bfb8297a02d1ae5d1
superozing <127123002+superozing@users.noreply.github.com>
2025-09-01 15:38:21 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,91 +1,52 @@
-///////////////////////////////////////////
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <list>
-#include <stack>
-#include <set>
-#include <map>
-#include <string>
-#include <queue>
-#include <deque>
-#include <unordered_map>
-#include <unordered_set>
-#include <bitset>
 #include <cmath>
+
+#include <vector>
 
 using namespace std;
 
-#define endl ("\n")
-
-enum color
-{
-    r, g, b,
-};
-
-class Boj
-{
-private:
-    int N; // 집의 수
-
-    vector<vector<int>> RGB; // 비용
-
-public:
-
-    void input()
-    {
-        cin >> N;
-        RGB.resize(N + 1, vector<int>(3));
-
-        for (int i = 1; i <= N; ++i)
-        {
-            for (int j = 0; j < 3; ++j)
-                cin >> RGB[i][j];
-        }
-    }
-
-    void progress()
-    {
-        /*
-        1번 집의 색은 2번 집의 색과 같지 않아야 한다.
-        N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
-        i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
-        */
-
-        // RGRGRGRG 이런 식으로 해도 된다..
-        // R -> G 또는 B로 나아갈 수 있다.
-        // R 입장에서는 G와 B 중 최소로 나아가야 한다.
-
-        for (int i = 2; i <= N; ++i)
-        {
-            RGB[i][r] += min(RGB[i - 1][g], RGB[i - 1][b]);
-            RGB[i][g] += min(RGB[i - 1][r], RGB[i - 1][b]);
-            RGB[i][b] += min(RGB[i - 1][r], RGB[i - 1][g]);
-        }
-
-        int answer = 1e9;
-
-        for (int it : RGB[N])
-            answer = min(answer, it);
-
-        cout << answer;
-    }
-
-private:
-
-};
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    Boj boj;
+	// 빨, 초, 파 중 하나의 색으로 집을 칠할 수 있다
+	// 각 집마다 칠하는 비용이 있다.
+	// 모든 집을 규칙에 맞게 칠했을 때 최소 비용 구하기.
 
-    boj.input();
-    boj.progress();
+	/* 규칙
+		1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+		N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+		i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+	*/
 
-    return 0;
+	int N;
+	cin >> N;
+
+	vector<int[3]> houses(N);
+
+	for (auto& house : houses)
+	{
+		for (int i = 0; i < 3; ++i)
+			cin >> house[i];
+	}
+
+	// dp[i][j] = i 번째 집에 색상 j를 반드시 칠할 경우, 칠할 수 있는 최소값
+
+	for (int i = 1; i < houses.size(); ++i)
+	{
+		auto& curHouse = houses[i];
+		auto& prevHouse = houses[i - 1];
+
+		curHouse[0] += min(prevHouse[1], prevHouse[2]);
+		curHouse[1] += min(prevHouse[0], prevHouse[2]);
+		curHouse[2] += min(prevHouse[0], prevHouse[1]);
+	}
+
+	auto& answerHouse = houses.back();
+
+	cout << min(min(answerHouse[0], answerHouse[1]), answerHouse[2]);
+
+	return 0;
 }
-
