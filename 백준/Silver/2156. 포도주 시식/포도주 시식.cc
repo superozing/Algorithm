2c4b3ce1361b49737bec29b0a3b071fb0ae189a4
superozing <127123002+superozing@users.noreply.github.com>
2025-09-04 21:33:59 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,81 +1,63 @@
 #include <iostream>
-#include <vector>
-
 #include <algorithm>
+#include <cmath>
+
+#include <vector>
 
 using namespace std;
 
-class BAEKJOON // 포도주 시식
-{
-public:
-    int         N;      // 포도주 수
-    vector<int> wine;   // 포도주 벡터
-    vector<int> dp;     // dp[n] => wine[n]까지 마셨을 때 최대로 마실 수 있는 포도주의 양
-
-public:
-    BAEKJOON() { init(); }
-    void init();
-    void progress();
-
-private:
-};
-
-void BAEKJOON::init()
-{
-    cin >> N;
-
-    wine.resize(N + 1, 0);
-    dp.resize(N + 1, 0);
-
-    // 포도주 벡터 채우기
-    for (int i = 1; i < wine.size(); ++i)
-        cin >> wine[i];
-}
-
-
-void BAEKJOON::progress()
-{
-    // dp의 초기값 설정
-    dp[0] = 0;
-    dp[1] = wine[1];
-    dp[2] = wine[1] + wine[2];
-
-    // 그러면 dp[3]은 무엇일까...?
-    // dp[3] = (wine[1] + wine[2]) , (wine[1] + wine[3]) , (wine[2] + wine[3]) 중에 max
-
-    // 그러면 dp[4]는 무엇일까...?
-    // dp[4] = (wine[1] + wine[2] + wine[4]) 또는 (wine[1] + wine[3] + wine[4]) 또는 (wine[2] + wine[3]) 중에 max
-
-    // 이걸 정리하면
-    //      (wine[2] + wine[3]) => dp[3]에 포함되어 있는 3가지(wine[1] + wine[2],  wine[1] + wine[3],  wine[2] + wine[3]) 중 하나. 
-    //              wine[1] + wine[2], wine[1] + wine[3]의 경우에는 무조건 wine[4]를 더한 값이 크기 때문에 
-    //              이전 dp[3]의 값이 wine[2] + wine[3]이었을 경우가 클 때를 위해서 체크함.
-    //              그래서 dp[3]으로 치환 가능
-    //      (wine[1] + wine[2] + wine[4]) => dp[2]에 wine[4]를 더한 값. 
-    //      (wine[1] + wine[3] + wine[4]) => dp[1]에 wine[3] + wine[4]를 더한 값. 
-    // 
-    //      
-    // dp[4] = max( { dp[3], dp[2] + wine[4], dp[1] + wine[3] + wine[4] } ); 
-
-
-    // 이걸 i로 바꾸면
-    // dp[i] = max( { dp[i - 1], dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i] } );
-
-
-    for (int i = 3; i < dp.size(); ++i)
-    {
-        dp[i] = max({ dp[i - 1]
-                    , dp[i - 2] + wine[i]
-                    , dp[i - 3] + wine[i - 1] + wine[i] });
-    }
-
-    cout << dp.back();
-}
-
 int main()
 {
-    BAEKJOON b;
-    b.progress();
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    return 0;
+	/*
+	포도주 잔을 선택하면 그 잔에 들어있는 포도주는 모두 마셔야 하고, 
+	마신 후에는 원래 위치에 다시 놓아야 한다.
+	연속으로 놓여 있는 3잔을 모두 마실 수는 없다.
+	*/
+
+	// 계단 오르기가 생각나는 문제에요.
+
+	int N;
+	cin >> N;
+
+	vector<int> wines(N);
+	for (auto& i : wines)
+		cin >> i;
+
+	// dp[i] => i 번째까지 규칙에 따라 마셨을 때 마실 수 있는 최대 포도주 양
+
+	switch (N)
+	{
+	case 1:
+		cout << wines[0];
+		return 0;
+
+	case 2:
+		cout << wines[0] + wines[1];
+		return 0;
+
+	case 3:
+		cout << max(wines[0] + wines[1], max(wines[0], wines[1]) + wines[2]);
+		return 0;
+	}
+
+	int answer = 0;
+	vector<int> dp(N);
+	dp[0] = wines[0];
+	dp[1] = wines[0] + wines[1];
+	dp[2] = max(dp[1], max(wines[0], wines[1]) + wines[2]);
+
+	for (int i = 3; i < N; ++i)
+	{
+		// 1. i - 3, i - 1, i 를 마시기
+		// 2. i - 2, i 를 마시기
+		// 3. i를 안마시기
+		dp[i] = max(dp[i - 1], max(dp[i - 2], dp[i - 3] + wines[i - 1]) + wines[i]);
+	}
+
+	cout << *max_element(begin(dp), end(dp));
+
+	return 0;
 }
