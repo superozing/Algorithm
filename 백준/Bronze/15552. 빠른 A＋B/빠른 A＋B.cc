06f211a1a173ecd825d717e10bd27e764c6fb1c7
superozing <127123002+superozing@users.noreply.github.com>
2025-10-06 21:24:53 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL); 
+    ios::sync_with_stdio(false);
+    
+    int N;
+    cin >> N;
+    
+    while (N--)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+        cout << a + b << '\n';
+    }
+    
+    return 0;
+}
