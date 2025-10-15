f0c640dc625b7163eb55978f7be7e77f06df432f
superozing <127123002+superozing@users.noreply.github.com>
2025-10-15 21:23:35 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    
+    cout << (a.length() >= b.length() ? "go" : "no");
+    
+    return 0;
+}
