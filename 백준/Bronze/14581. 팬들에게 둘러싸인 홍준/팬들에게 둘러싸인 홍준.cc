2024b476969d24740d3d93afa73e99082add4746
superozing <127123002+superozing@users.noreply.github.com>
2025-10-21 22:16:34 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <queue>
+
+#define endl ('\n')
+
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    string s;
+    cin >> s;
+    cout << ":fan::fan::fan:\n:fan::" << s << "::fan:\n:fan::fan::fan:";
+
+    return 0;
+}
