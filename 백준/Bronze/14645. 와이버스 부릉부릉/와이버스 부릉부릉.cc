84dd7fe58b97a0a08a549455af947ecf30fc6c0a
superozing <127123002+superozing@users.noreply.github.com>
2025-11-02 22:24:20 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    
+    int in;
+    for (int i = 0; i < N * M; ++i)
+        cin >> in;
+    
+    cout << "비와이";
+    
+    return 0;
+}
