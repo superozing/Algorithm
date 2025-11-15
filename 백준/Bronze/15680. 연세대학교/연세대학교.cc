30ee09c55de3d8d08391682a740dac708c0583fa
superozing <127123002+superozing@users.noreply.github.com>
2025-11-15 23:41:18 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    cout << (N ? "Leading the Way to the Future" : "YONSEI");
+    
+    
+    return 0;
+}
